24b6563710d1b96375d18cf4aa2e77755f7545c6
FrederikGJ <113090989+FrederikGJ@users.noreply.github.com>
2022-12-09 10:29:17 +0100

    Create OBJRam.java
    ny klasse lavet . billede mangler så den bruger et andet bilede lige pt

---
+++ src/object/OBJRam.java
@@ -0,0 +1,22 @@
+package object;
+
+import javax.imageio.ImageIO;
+import java.io.IOException;
+
+public class OBJRam extends SuperObject
+{
+
+    public OBJRam()
+    {
+        name = "Sign";
+        try
+        {
+            image = ImageIO.read(getClass().getResourceAsStream("/objects/JesperFrontSmoke1.png"));
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        collision=false;
+    }
+}
