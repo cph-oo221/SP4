f153cb58affe50df98c0d9246c5058633a8bf6e8
Sebbedeb <zor.seb.0901@gmail.com>
2022-12-13 09:48:05 +0100

    RAM-billede implementeret

--- src/main/UI.java
+++ src/main/UI.java
@@ -1,6 +1,7 @@
 package main;
 
 import object.OBJGraphicsCard;
+import object.OBJRam;
 
 import java.awt.*;
 
@@ -12,21 +13,26 @@ public class UI
 
     GamePanel gp;
     Font arial_BOLD_40;
-    BufferedImage tempImage;
+    BufferedImage graphicsCardImage;
+    BufferedImage RAMimage;
     public UI(GamePanel gp)
     {
         this.gp = gp;
         arial_BOLD_40 = new Font("Arial" , Font.BOLD , 40);
         OBJGraphicsCard OBJGraphicsCard = new OBJGraphicsCard();
-        tempImage = OBJGraphicsCard.image;
+        OBJRam OBJRAM = new OBJRam();
+        graphicsCardImage = OBJGraphicsCard.image;
+        RAMimage = OBJRAM.image;
     }
 
     public void draw(Graphics2D g2)
     {
+
         g2.setFont(arial_BOLD_40);
         g2.setColor(Color.white);
-        g2.drawImage(tempImage , gp.tileSize/2 , gp.tileSize/2 , gp.tileSize, gp.tileSize, null);
-        g2.drawImage(tempImage , gp.tileSize/2 , gp.tileSize/2 , gp.tileSize, gp.tileSize, null);
+        g2.drawImage(graphicsCardImage , gp.tileSize/2 , gp.tileSize/2 , gp.tileSize, gp.tileSize, null);
         g2.drawString(": "+gp.player.hasGraphicsCard, 80, 67);
+        g2.drawImage(RAMimage , gp.tileSize/2 , (int) (1.3*gp.tileSize) , gp.tileSize, gp.tileSize, null);
+        g2.drawString(": "+gp.player.hasGraphicsCard, 80, 67+48);
     }
 }
